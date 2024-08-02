d1d17040523c19686e1b515e60b59470e9a487a8
bandi@DESKTOP-OFQ0D03 <kiran.bandi1995@gmail.com>
2024-08-02 08:14:30 +0530

    commit 02/08

--- RNBootCamp/src/Redux/ReduxToolKit/balanceSlice.js
+++ RNBootCamp/src/Redux/ReduxToolKit/balanceSlice.js
@@ -3,14 +3,14 @@ import { createSlice } from '@reduxjs/toolkit';
 export const balanceSlice = createSlice({
   name: 'balance',
   initialState: {
-    value: Number(10000),
+    value: 10000,
   },
   reducers: {
     deposit: (state,action) => {
-      state.value += Number  (action.payload);
+      state.value += action.payload;
     },
     withdraw: (state,action) => {
-      state.value -= Number(action.payload);
+      state.value -= action.payload;
     },
     
   }, 
--- RNBootCamp/src/Redux/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import balanceReducer from './balanceSlice';
-
-export const store = configureStore({
-  reducer: {
-    balance: balanceReducer,
-  },
-});
